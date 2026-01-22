303c557a6a6d8670c755cef1272d17db2a8bf8a1
Agustina Serrano <camilaserrano2001@gmail.com>
2026-01-21 23:29:27 -0300

    Persist tasks using localStorage

--- src/main.js
+++ src/main.js
@@ -1,14 +1,47 @@
 const input = document.getElementById("task-input");
-const button = document.getElementById("add-task-btn");
+const addButton = document.getElementById("add-task-btn");
 const list = document.getElementById("task-list");
+const clearButton = document.getElementById("clear-done-btn");
+
+// función para guardar todas las tareas en localStorage
+function saveTasks() {
+    const tasks = [];
+    document.querySelectorAll("#task-list li").forEach(li => {
+        tasks.push({
+            text: li.textContent,
+            done: li.classList.contains("done")
+        });
+    });
+    localStorage.setItem("tasks", JSON.stringify(tasks));
+}
+
+// función para cargar las tareas desde localStorage
+function loadTasks() {
+    const tasks = JSON.parse(localStorage.getItem("tasks")) || [];
+    
+    tasks.forEach(task => {
+        const li = document.createElement("li");
+        li.textContent = task.text;
+
+        if(task.done) li.classList.add("done");
+
+        // elementos tachados
+        li.addEventListener("click", () => {
+            li.classList.toggle("done");
+            saveTasks();
+        });
+
+        list.appendChild(li);
+    });
+}
+
+// cargar tareas al iniciar página
+loadTasks();
 
 // botón para agregar tareas
-button.addEventListener("click", () => {
-    const taskText = input.value;
-
-    if (taskText === ""){
-        return;
-    }
+addButton.addEventListener("click", () => {
+    const taskText = input.value.trim();
+    if (taskText === "") return;
 
     const li = document.createElement("li");
     li.textContent = taskText;
@@ -16,16 +49,17 @@ button.addEventListener("click", () => {
     // interacción de tachado de elementos
     li.addEventListener("click", () => {
         li.classList.toggle("done");
+        saveTasks();
     });
 
     list.appendChild(li);
     input.value = "";
+
+    saveTasks();
 });
 
 // botón para borrar tareas completadas
-const clearButton = document.getElementById("clear-done-btn");
-
 clearButton.addEventListener("click", () => {
-    const tasks = document.querySelectorAll("#task-list .done");
-    tasks.forEach(task => task.remove());
-})
+    document.querySelectorAll("#task-list .done").forEach(task => task.remove())
+    saveTasks();
+});
